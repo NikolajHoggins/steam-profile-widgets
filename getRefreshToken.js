08a7debc4d5f4601521a1175145ae3117c88af67
Tsukani <34612305+Tsukani@users.noreply.github.com>
2020-09-09 17:38:36 +0200

    Fixed login issues & added interval speed

--- getRefreshToken.js
+++ getRefreshToken.js
@@ -1,48 +1,32 @@
 //Libraries
-const request = require("request");
-const btoa = require("btoa");
-const config = require("./config.json");
-const dotenv = require("dotenv");
-dotenv.config();
+const request = require('request');
+const btoa = require('btoa');
+const config = require('./config.json');
 
 const colors = {
-  red: "\x1b[31m%s\x1b[0m",
-  purple: "\x1b[35m%s\x1b[0m",
-  green: "\x1b[32m%s\x1b[0m",
-};
+    red: "\x1b[31m%s\x1b[0m",
+    purple: "\x1b[35m%s\x1b[0m",
+    green: "\x1b[32m%s\x1b[0m"
+}
 
 console.log(colors.purple, "Aquireing Refresh Token...");
 var headers = {
-  Authorization:
-    "Basic " +
-    btoa(
-      `${process.env.SPOTIFY_CLIENT_ID || "id"}:${
-        process.env.SPOTIFY_CLIENT_SECRET || "secret"
-      }`
-    ),
-  "Content-Type": "application/x-www-form-urlencoded",
+    'Authorization': 'Basic ' + btoa(`${config.client_id}:${config.client_secret}`),
+    "Content-Type": "application/x-www-form-urlencoded",
 };
-
+    
 var options = {
-  url: `https://accounts.spotify.com/api/token`,
-  method: "POST",
-  body: `grant_type=authorization_code&code=${
-    process.env.SPOTIFY_AUTH_CODE || "code"
-  }&redirect_uri=https://spotify.com`,
-  headers: headers,
+    url: `https://accounts.spotify.com/api/token`,
+    method: "POST",
+    body: `grant_type=authorization_code&code=${config.authorizationCode}&redirect_uri=https://spotify.com`,
+    headers: headers,
 };
 
 request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    refreshToken = JSON.parse(body).refresh_token;
-    console.log(
-      colors.green,
-      `Successfully aquired Refresh Token!\n${refreshToken}`
-    );
-  } else {
-    return console.log(
-      colors.red,
-      `An error occured while renewing Access Token.\n${body}`
-    );
-  }
+    if (!error && response.statusCode == 200) {
+        refreshToken = JSON.parse(body).refresh_token;
+        console.log(colors.green, `Successfully aquired Refresh Token!\n${refreshToken}`);
+    } else {
+        return console.log(colors.red, `An error occured while renewing Access Token.\n${body}`);
+    }
 });
--- index.js
+++ index.js
@@ -1,228 +1,162 @@
 //Libraries
-const SteamUser = require("steam-user");
-const SteamTotp = require("steam-totp");
-const request = require("request");
-const btoa = require("btoa");
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const request = require('request');
+const btoa = require('btoa');
 const client = new SteamUser();
-const dotenv = require("dotenv");
-const config = require("./config.json");
+const config = require('./config.json');
 
-dotenv.config();
 
 const colors = {
-  red: "\x1b[31m%s\x1b[0m",
-  purple: "\x1b[35m%s\x1b[0m",
-  green: "\x1b[32m%s\x1b[0m",
-};
+    red: "\x1b[31m%s\x1b[0m",
+    purple: "\x1b[35m%s\x1b[0m",
+    green: "\x1b[32m%s\x1b[0m"
+}
 
 //Account Credentials
 const logOnOptions = {
-  accountName: process.env.STEAM_ACCOUNT_NAME || "user",
-  password: process.env.STEAM_ACCOUNT_PASS || "pass",
-  twoFactorCode: SteamTotp.generateAuthCode(
-    process.env.STEAM_ACCOUNT_SHARED_SECRET || "secret"
-  ),
+  accountName: config.steamAccount.username,
+  password: config.steamAccount.password,
+  twoFactorCode: SteamTotp.generateAuthCode(config.steamAccount.sharedSecret)
 };
 
 function renewOAuth() {
-  console.log(colors.purple, "Aquireing Access Token...");
-  var refreshHeaders = {
-    Authorization:
-      "Basic " +
-      btoa(
-        `${process.env.SPOTIFY_CLIENT_ID || "id"}:${
-          process.env.SPOTIFY_CLIENT_SECRET || "secret"
-        }`
-      ),
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
-
-  var refreshOptions = {
-    url: `https://accounts.spotify.com/api/token`,
-    method: "POST",
-    body: `grant_type=refresh_token&refresh_token=${
-      process.env.SPOTIFY_REFRESH_TOKEN || "token"
-    }`,
-    headers: refreshHeaders,
-  };
-  request(refreshOptions, function (
-    refreshError,
-    refreshResponse,
-    refreshBody
-  ) {
-    if (!refreshError && refreshResponse.statusCode == 200) {
-      accessToken = JSON.parse(refreshBody).access_token;
-      console.log(
-        colors.green,
-        `Successfully aquired Access Token! (${accessToken})`
-      );
-      return logOnSteam(accessToken);
-    } else {
-      return console.log(
-        colors.red,
-        "An error occured while renewing Access Token. Please aquire a new Refresh Token and try again."
-      );
-    }
-  });
+    console.log(colors.purple, "Aquireing Access Token...");
+    var refreshHeaders = {
+        'Authorization': 'Basic ' + btoa(`${config.spotify.client_id}:${config.spotify.client_secret}`),
+        "Content-Type": "application/x-www-form-urlencoded",
+    };
+    
+    var refreshOptions = {
+        url: `https://accounts.spotify.com/api/token`,
+        method: "POST",
+        body: `grant_type=refresh_token&refresh_token=${config.spotify.refreshToken}`,
+        headers: refreshHeaders,
+    };
+    request(refreshOptions, function (refreshError, refreshResponse, refreshBody) {
+        if (!refreshError && refreshResponse.statusCode == 200) {
+            accessToken = JSON.parse(refreshBody).access_token;
+            console.log(colors.green, `Successfully aquired Access Token! (${accessToken})`);
+            if (!client._sessionID) logOnSteam();
+            return;
+        } else {
+            return console.log(colors.red, "An error occured while renewing Access Token. Please aquire a new Refresh Token and try again.");
+        }
+    });
 }
 
-function logOnSteam(accessToken) {
-  //Login
-  client.logOn(logOnOptions);
-  console.log(colors.purple, "Attempting to login to Steam...");
-  client.on("loggedOn", () => {
-    console.log(colors.green, "Successfully logged into Steam!");
-  });
+function logOnSteam() {
+    //Login
+    client.logOn(logOnOptions);
+    console.log(colors.purple, "Attempting to login to Steam...");
+    client.on("loggedOn", () => {
+        console.log(colors.green, "Successfully logged into Steam!");
+    });
 
-  //Login Error
-  client.on("error", function (e) {
-    return console.log(colors.red, `Failed to login: ${e.toString()}`);
-  });
+    //Login Error
+    client.on('error', function(e) {
+        return console.log(colors.red, `Failed to login: ${e.toString()}`);
+    });
 
-  //Get webSession (sessionID & Cookies) and initiate main function
-  client.on("webSession", function (sessionID, cookies) {
-    console.log(colors.green, "Successfully acquired sessionID and Cookies!");
-    updateSpotify(sessionID, cookies);
-    setInterval(() => {
-      updateSpotify(sessionID, cookies);
-    }, 30000);
-  });
+    //Get webSession (sessionID & Cookies) and initiate main function
+    client.on("webSession", function(sessionID, cookies) {
+        console.log(colors.green, "Successfully acquired sessionID and Cookies!");
+        updateSpotify(sessionID, cookies);
+        setInterval(() => {
+            updateSpotify(sessionID, cookies);
+        }, Number(config.updateDelay*1000));  
+    });
+}
 
-  var progressBar = [
-    "◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬",
-    "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯",
-  ];
+function updateSpotify(sessionID, cookies) {
+    var progressBar = [
+        "◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯▬",
+        "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬◯"
+    ];
 
-  function updateSpotify(sessionID, cookies) {
     try {
-      var spotifyHeaders = {
-        Accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Bearer ${accessToken}`,
-      };
-
-      var spotifyOptions = {
-        url: `https://api.spotify.com/v1/me/player/currently-playing`,
-        method: "GET",
-        headers: spotifyHeaders,
-      };
-      request(spotifyOptions, function (
-        spotifyError,
-        spotifyResponse,
-        spotifyBody
-      ) {
-        try {
-          data = JSON.parse(spotifyBody);
-        } catch (error) {
-          //dø
-        }
-        if (!spotifyError && spotifyResponse.statusCode == 200) {
-          try {
-            steamBoxString = `[b]Currently listning to Spotify:[/b]\n[url=${
-              data.item.external_urls.spotify
-            }]${data.item.artists[0].name} - ${data.item.name}[/url]\n${
-              data.is_playing ? "▶" : "❚❚"
-            } ${secondsToMinutesSeconds(
-              (Number(data.progress_ms) / 1000).toFixed(0)
-            )} / ${secondsToMinutesSeconds(
-              (Number(data.item.duration_ms) / 1000).toFixed(0)
-            )} ${
-              progressBar[
-                Math.floor(
-                  ((Number(data.progress_ms) / 1000).toFixed(0) /
-                    (Number(data.item.duration_ms) / 1000).toFixed(0)) *
-                    20
-                )
-              ]
-            }`;
-          } catch (e) {
-            steamBoxString = `Currently not listening to Spotify.`;
-          }
-        } else if (spotifyResponse.statusCode == 401) {
-          renewOAuth();
-          return console.log(
-            colors.red,
-            "OAuth token has expired. Renewing..."
-          );
-        } else {
-          steamBoxString = `Currently not listening to Spotify.`;
-        }
-
-        var steamHeaders = {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Cookie: cookies,
-        };
-        var steamOptions = {
-          url: `https://steamcommunity.com/profiles/${
-            process.env.STEAM_64 || "id"
-          }/edit`,
-          method: "POST",
-          headers: steamHeaders,
-          body: config.profileData
-            .replace("SESID", sessionID)
-            .replace("SPOTIFYSTATUS", steamBoxString.replace(/&/g, "%26")),
-          gzip: true,
+        var spotifyHeaders = {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": `Bearer ${accessToken}` 
         };
 
-        function secondsToMinutesSeconds(d) {
-          var minutes = Math.floor((d % 3600) / 60);
-          var seconds = Math.floor((d % 3600) % 60);
-          if (seconds <= 9) seconds = "0" + seconds;
-          return minutes + ":" + seconds;
-        }
-        d = new Date();
-        time = d.toLocaleString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-        });
-        request(steamOptions, function (steamError, steamResponse, steamBody) {
-          if (!steamError && steamResponse.statusCode == 200) {
-            if (JSON.parse(steamBody).success == "1") {
-              console.log(
-                colors.green,
-                `[${time}] Showcase updated. [${
-                  data.item
-                    ? `${data.item.artists[0].name} - ${data.item.name}`
-                    : "Ad break"
-                }]`
-              );
+        var spotifyOptions = {
+            url: `https://api.spotify.com/v1/me/player/currently-playing`,
+            method: "GET",
+            headers: spotifyHeaders
+        };
+        request(spotifyOptions, function (spotifyError, spotifyResponse, spotifyBody) {
+            if (!spotifyBody) {
+                steamBoxString = `Currently not listening to Spotify.`;
             } else {
-              console.log(
-                colors.red,
-                `[${time}] Failed to update showcase. Please check your profileData.`
-              );
+                data = JSON.parse(spotifyBody);
+                if (!spotifyError && spotifyResponse.statusCode == 200) {
+                    try {
+                        steamBoxString = `[b]Currently listning to Spotify:[/b]\n[url=${data.item.external_urls.spotify}]${data.item.artists[0].name} - ${data.item.name}[/url]\n${data.is_playing ? "▶" : "❚❚"} ${secondsToMinutesSeconds((Number(data.progress_ms)/1000).toFixed(0))} / ${secondsToMinutesSeconds((Number(data.item.duration_ms)/1000).toFixed(0))} ${progressBar[Math.floor(((Number(data.progress_ms)/1000).toFixed(0)/(Number(data.item.duration_ms)/1000).toFixed(0))*20)]}`;
+                    } catch(e){
+                        steamBoxString = `Currently not listening to Spotify.`;
+                    }        
+                } else if (spotifyResponse.statusCode == 401) {
+                    renewOAuth();
+                    return console.log(colors.red, "OAuth token has expired. Renewing...");
+                } else {
+                    steamBoxString = `Currently not listening to Spotify.`;
+                }
             }
-          } else {
-            console.log(
-              colors.red,
-              `[${time}] Failed to load showcase status. Status code: ${steamResponse.statusCode}`
-            );
-          }
+
+            var steamHeaders = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Cookie": cookies
+            };
+            var steamOptions = {
+                url: `https://steamcommunity.com/profiles/${config.steamAccount.steamID64}/edit`,
+                method: "POST",
+                headers: steamHeaders,
+                body: config.steamAccount.profileData.replace("SESID", sessionID).replace("SPOTIFYSTATUS", steamBoxString.replace(/&/g, '%26')),
+                gzip: true
+            };
+                
+            function secondsToMinutesSeconds(d) {
+                var minutes = Math.floor(d % 3600 / 60);
+                var seconds = Math.floor(d % 3600 % 60);
+                if (seconds <= 9) seconds = "0" + seconds;
+                return minutes + ":" + seconds;
+            }
+            d = new Date();
+            time = d.toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"});
+            request(steamOptions, function (steamError, steamResponse, steamBody) {
+                if (!steamError && steamResponse.statusCode == 200) {
+                    if (JSON.parse(steamBody).success == "1") {
+                        if (spotifyBody) {console.log(colors.green, `[${time}] Showcase updated. [${data.item ? `${data.item.artists[0].name} - ${data.item.name}` : "Ad break"}]`);}
+                        else {console.log(colors.green, `[${time}] Showcase updated. [Not playing]`);}
+                    } else {
+                        console.log(colors.red, `[${time}] Failed to update showcase. Please check your profileData.`);
+                    }
+                } else {
+                    console.log(colors.red, `[${time}] Failed to load showcase status. Status code: ${steamResponse.statusCode}`);
+                }
+            });
         });
-      });
-    } catch (e) {
-      console.log(colors.red, `An error occured: ${e}`);
-    }
-  }
+    } catch(e){console.log(colors.red, `An error occured: ${e}`);}
 }
 
 renewOAuth();
